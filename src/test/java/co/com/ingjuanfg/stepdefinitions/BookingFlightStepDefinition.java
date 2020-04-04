2fdd7686f05ee95d528cff5d837b49152ef8b519
ingjuanfg <juan.isfg@gmail.com>
2020-04-04 17:28:01 -0500

    Se realzia mapeo, tassk para clase Flight}

--- src/test/java/co/com/ingjuanfg/stepdefinitions/BookingFlightStepDefinition.java
+++ src/test/java/co/com/ingjuanfg/stepdefinitions/BookingFlightStepDefinition.java
@@ -1,21 +1,27 @@
 package co.com.ingjuanfg.stepdefinitions;
 
-import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
-
 import co.com.ingjuanfg.interactions.OpenBrowser;
 import co.com.ingjuanfg.tasks.Search;
-import co.com.ingjuanfg.tasks.Car;
+import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.actions.Click;
+
+import javax.xml.crypto.Data;
+import java.util.List;
+import java.util.Map;
+
+import static co.com.ingjuanfg.user_interfaces.HomePage.TXT_DATE;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
 
 public class BookingFlightStepDefinition {
 
   @Given("^that (.*) searchs the flight options$")
-  public void thatTheUserWantsToLookSomeFlights(String nameActor) {
-    theActorCalled(nameActor).wasAbleTo(OpenBrowser.withSpecificUrl(),
-            Search.flight(),
-            Search.car()
+  public void thatTheUserWantsToLookSomeFlights(String nameActor, Map<String, String> detailsFlight) {
+    theActorCalled(nameActor).wasAbleTo(
+            OpenBrowser.withSpecificUrl(),
+            Search.flight(detailsFlight)
     );
   }
 
